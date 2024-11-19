b058e1c3923d6a25f636dcce55edd7d58af98bfe
unknown <teodor.vatajita@yahoo.com>
2024-11-19 19:18:11 +0200

    camera

---
+++ camera.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+def get_camera():
+    cam = cv.VideoCapture(0)
+
+    frame_width = int(cam.get(cv.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cam.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+    while(1):
+        _, frame = cam.read()
+        cv.imshow("CAMERA", frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+        if cv.waitKey(32) == ord(' '):
+            cv.imwrite("food.jpg",frame)
+
+
+    cam.release()
+    cv.destroyAllWindows()
---
+++ main.py
@@ -0,0 +1,8 @@
+#from rec import food_scan
+from camera import get_camera
+
+get_camera()
+
+# loc="C:\\Hackathon\\HackathonFoodRec\\food.jpg"
+# food_item,scan_confidence=food_scan(loc)
+# print(f'{food_item}, {scan_confidence}')
