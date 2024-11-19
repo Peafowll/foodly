9c246e8210138adb5d255f6d28be07df3dda08dc
Peafowl <tudorpaun16@gmail.com>
2024-11-19 19:40:40 +0200

    image-based object recognition

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 from rec import food_scan
-#from camera import get_camera
-
-#get_camera()
-
-loc="C:\\Hackathon\\HackathonFoodRec\\food.jpg"
-food_item,scan_confidence=food_scan(loc)
-print(f'{food_item}, {scan_confidence}')
-=======
-#from rec import food_scan
 from camera import get_camera
 
 get_camera()
 
-# loc="C:\\Hackathon\\HackathonFoodRec\\food.jpg"
-# food_item,scan_confidence=food_scan(loc)
-# print(f'{food_item}, {scan_confidence}')
->>>>>>> b058e1c3923d6a25f636dcce55edd7d58af98bfe
+loc="C:\\Hackathon\\HackathonFoodRec\\food.jpg"
+food_item,scan_confidence=food_scan(loc)
+print(f'{food_item}, {scan_confidence}')
