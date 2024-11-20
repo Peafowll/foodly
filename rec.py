eeb1b9e7a323a753b459fbdd4bf969834c0434bd
Peafowl <tudorpaun16@gmail.com>
2024-11-20 16:32:40 +0200

    comments

--- rec.py
+++ rec.py
@@ -5,23 +5,28 @@ from datetime import datetime
 import cv2   
 from google.cloud import vision_v1p3beta1 as vision
 
-allowed_foods=[line.rstrip('\n ').lower() for line in open('food.list')]
+allowed_foods=[line.rstrip('\n ').lower() for line in open('food.list')]#opens a list of allowed food items
 def food_scan(continut,client):
-    firsttime=datetime.now()
-    global allowed_foods
+    '''
+        args : 
+        continut = the google vision content parsed to function (in bytes)
+        client = the google vision API client object
+    '''
+    firsttime=datetime.now()#starts a timer for efficieny tracking
+    global allowed_foods #parses the list
     def find_food(continut,food_list):
-        image=vision.types.Image(content=continut)
-        response = client.label_detection(image=image)
-        labels = response.label_annotations #memorizes each prediction and its lieklyhood
-        for label in labels:
+        image=vision.types.Image(content=continut) #turns content to an image
+        response = client.label_detection(image=image) 
+        labels = response.label_annotations #memorizes each prediction and its likelyhood
+        for label in labels: #goes thru all the potential predictions seen
             desc = label.description.lower()
             score = round (label.score,2)
-            if desc in food_list:
+            if desc in food_list: #checks if it detected an allowed object
                 showable_label=desc.split(" ")[0]# ORIBIL
-                return showable_label, score
-            if score <=0.65:
+                return showable_label, score # returns the object and the confidence
+            if score <=0.65: #checks if the AI is confient
                 break
-        return "food not found",0
+        return "food not found",0 #if it doesnt find a valid object, returns so
     item,confidence=find_food(continut,allowed_foods)
-    #print(datetime.now()-firsttime)
-    return item,confidence
+    #print(datetime.now()-firsttime) # (FOR EFFICICENY TESTING)
+    return item,confidence # returns the found item and confidence
--- test_recognition.py
+++ test_recognition.py
@@ -7,62 +7,76 @@ import time
 from rec import food_scan
 
 def live():
-    cam = cv.VideoCapture(0)
+    #initiate the camera and sizes
+    cam = cv.VideoCapture(0) 
     cam.set(cv.CAP_PROP_FRAME_WIDTH, 640)
     cam.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
 
     #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'client_key.json'
     #client = vision.ImageAnnotatorClient()
 
-    showable_label = "Loading..."
-    lock = Lock()
+    showable_label = "Loading..." # initiate the showing label with "Loading..."
+    #sum threadding for efficiency
+    lock = Lock() 
     frame_queue = Queue(maxsize=5)
+    #initializing the google vision client
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'client_key.json'
     client=vision.ImageAnnotatorClient()
+    #a function for transforming byte data into the detected object and confidence for each frame
     def process_frames():
         nonlocal showable_label
         while True:
-            content = frame_queue.get()
+            content = frame_queue.get() #getting the byte data from the queue
             if content is None:
                 break
-            food_item,scan_confidence=food_scan(content,client)
-            showable_label=f"{food_item},{scan_confidence}"
+            food_item,scan_confidence=food_scan(content,client) # transfering the data and getting the info
+            showable_label=f"{food_item},{scan_confidence}" #making it a string
 
+    # more threadding 
     worker = Thread(target=process_frames, daemon=True)
     worker.start()
 
+    #ofc we had to do an fps counter
     fps=0
     prev_time = time.time()
     while True:
+        # sleeping for efficiency (too fast != good)
         time.sleep(0.02)
         ret, frame = cam.read()
+        #getting the frames and then flipping them
         frame = cv.flip(frame,1)
         if not ret:
             break
         
-        
+        #encodding the frames
         success, encoded_frame = cv.imencode(".jpg", frame, [cv.IMWRITE_JPEG_QUALITY, 50])
         if success and not frame_queue.full():
             content = encoded_frame.tobytes()
             frame_queue.put(content)
 
+        #the magic formula for fps
         current_time = time.time()
         elapsed_time = current_time-prev_time
         fps = 1/elapsed_time if elapsed_time>0 else 0
         prev_time = current_time
 
+        #putting the text when sure we have info
         with lock:
             cv.putText(frame, showable_label, (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv.LINE_AA)
             cv.putText(frame, f"FPS: {int(fps)}", (50, 100), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv.LINE_AA)
 
+        #showing the frames lol
         cv.imshow("CAMERA", frame)
 
+        #quitting the app
         if cv.waitKey(1) & 0xFF == 27:
             break
-
+        
+        #taking a photo if we really want to tho
         if cv.waitKey(1) & 0xFF == 32:
             cv.imwrite("food.jpg", frame)
 
+    #exitting all other api, libraries etc
     frame_queue.put(None)
     worker.join()
     cam.release()
