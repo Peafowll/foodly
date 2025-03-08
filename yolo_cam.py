f96545da30923a5e5197dc262a3bffc697b36241
Peafowl <tudorpaun16@gmail.com>
2025-03-08 11:31:22 +0200

    first image rec with YOLO

--- yolo_cam.py
+++ yolo_cam.py
@@ -2,15 +2,18 @@ import ultralytics
 import cv2
 from ultralytics import YOLO
 model=YOLO("yolov8m.pt")
-capture= cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 while True:
     ret, frame = capture.read()
     if not ret:
         break
-    results = model(frame, show=True)
-    print(f'results={results}')
+    #cv2.imshow("frame",frame)
+    results = model(frame)
+    #print(f'results={results}')
     image_results = results[0].plot()
     cv2.imshow("AI Webcam",image_results)
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
+        break
 
 capture.release()
 cv2.destroyAllWindows()
